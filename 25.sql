79c9f886521b117f36ba6f6a4bd23a046b9b570f
Avani Maletha <125181112+Avanimaletha@users.noreply.github.com>
2025-01-31 21:22:44 +0530

    Update 25.sql
    hh

--- 25.sql
+++ 25.sql
@@ -1,3 +1,6 @@
-SELECT teacher_id, COUNT(DISTINCT(subject_id)) AS cnt
-FROM Teacher
-GROUP BY teacher_id
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE 
+  (product_id, year) 
+  IN (SELECT product_id, MIN(year) 
+  FROM Sales GROUP BY 1)
